0d220033c3266f660bf38250233e82cb8ac45b7b
lxc <18867762063@163.com>
2024-07-09 21:53:49 +0800

    ✨ feat(curd): 增加展开和收起
    可能某些页面搜索内容过多，有些不重要的可以收起来。

--- src/components/me/index.js
+++ src/components/me/index.js
@@ -1,3 +1,4 @@
-export { default as MeModal } from './modal/index.vue'
 export { default as MeCrud } from './crud/index.vue'
+export { default as MeCrud2 } from './crud/index2.vue'
 export { default as MeQueryItem } from './crud/QueryItem.vue'
+export { default as MeModal } from './modal/index.vue'
